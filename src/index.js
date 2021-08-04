037aa3a8d17ee3addcdc7165370c54d0e0275e75
duygugurbuzyildiz <gurbuz.duygu@gmail.com>
2021-08-04 03:04:53 +0200

    Github link added

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@ import App from './App';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <div className="ms-5 ps-4">
+      <p>
+        <a href="https://github.com/duygugurbuzyildiz/weather-project-react" target="_blank" rel="noreferrer">
+          Open-source code
+        </a>
+        , by Duygu Gurbuz Yildiz from 
+        <a href="https://www.shecodes.io/" target="_blank" rel="noreferrer">SheCodes
+        </a>.
+      </p>
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
